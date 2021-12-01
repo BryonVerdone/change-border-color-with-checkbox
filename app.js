bf7e5a9cb1ab5f567359288f0af9625f73dfa875
BryonVerdone <bryonverdone@gmail.com>
2021-11-30 21:38:00 -0500

    added green btn function

--- app.js
+++ app.js
@@ -5,10 +5,25 @@ const blueBtn = document.getElementById('blue');
 
 // images
 
+const imgOne = document.getElementById('grey');
+const imgTwo = document.getElementById('code');
+const imgThree = document.getElementById('desk');
 // need to set up add border to all images
 
-const images = document.querySelector('.img');
-
 redBtn.addEventListener('click', function () {
-  images.classList.add('border-red');
+  imgOne.classList.add('border-red');
+  imgTwo.classList.add('border-red');
+  imgThree.classList.add('border-red');
+  if (imgOne.classList.contains('border-green')) {
+    imgOne.classList.remove('border-green');
+  }
+});
+
+greenBtn.addEventListener('click', function () {
+  imgOne.classList.remove('border-red');
+  imgTwo.classList.remove('border-red');
+  imgThree.classList.remove('border-red');
+  imgOne.classList.add('border-green');
+  imgTwo.classList.add('border-green');
+  imgThree.classList.add('border-green');
 });
